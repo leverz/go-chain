2faaebc5d7bab81b2762c7e0cc5af662bd12033d
wangzheng <wangzheng@shimo.im>
2018-03-14 23:16:02 +0800

    feat: get a whole chain through a hash

--- chain.go
+++ chain.go
@@ -11,6 +11,11 @@ type Chain struct {
 	db *bolt.DB // open once use every where
 }
 
+type ChainIterator struct {
+	currentHash []byte
+	db *bolt.DB
+} 
+
 // 这里使用指针是因为希望直接修改 Chain 的 blocks 的值
 func (c *Chain) AddBlock (data string)  {
 	err := c.db.Update(func(tx *bolt.Tx) error {
@@ -59,4 +64,27 @@ func CreateBlockChain() Chain {
 	})
 	LogError("blotDB createBlockChain error", err)
 	return Chain{tip, db}
+}
+
+func (c *Chain) Iterator() ChainIterator {
+	return ChainIterator{c.tip, c.db}
+}
+
+func (i *ChainIterator) Next() Block {
+	var block Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		deserializeResult, err := DeserializeBlock(encodedBlock)
+		LogError("DeserializeBlock error", err)
+		block = deserializeResult
+
+		return nil
+	})
+
+	LogError("boltDB get block error", err)
+
+	i.currentHash = block.PrevBlockHash
+	return block
 }
